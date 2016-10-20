5e40ce95276cb2e683bb4e2f25151dac06e63ca4
Dominic Serrano <dmsrojo@gmail.com>
2016-10-20 15:08:47 -0500

    Register new user is now accepting json

---
+++ server/controllers/registerCtrl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const registerNewUser = (req, res) => {
+  console.log('Register new use ResJSON', req.body);
+};
+
+module.exports = { registerNewUser };
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,14 @@ const router = Router();
 const root = require('./rootRoute.js');
 const user = require('./userRoute.js');
 const login = require('./loginRoute.js');
+const register = require('./registerRoute.js');
 /////////////////////////////////////////
 
 //Routers
 router.use(root);
 router.use(user);
 router.use(login);
+router.use(register);
 
 /////////////////////////////////////////
 module.exports = router;
