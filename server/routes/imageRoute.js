00a3ccb72e03c9598e951b2cf4ac6177c9d7e81a
Dominic Serrano <dmsrojo@gmail.com>
2016-10-22 20:57:42 -0500

    Changed route names for image routes

--- server/routes/imageRoute.js
+++ server/routes/imageRoute.js
@@ -8,7 +8,7 @@ const { getEntireImageCollection, getSpecificImage, editSpecificImage, createNew
 router.post('/api/image/new', createNewImage);
 router.get('/api/image/:id', getSpecificImage);
 router.post('/api/image/:id', editSpecificImage);
-router.get('/api/image/all/collection', getEntireImageCollection);
-router.get('/api/image/all/:id', getSpeficUserImageCollection);
+router.get('/api/image/collection/all', getEntireImageCollection);
+router.get('/api/image/collection/:id', getSpeficUserImageCollection);
 
 module.exports = router;
