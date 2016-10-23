40ad76c76be95bb8a30626cf3585ef1bf6592e42
Dominic Serrano <dmsrojo@gmail.com>
2016-10-23 12:07:18 -0500

    Edited postDoc with the current post routes

--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -11,8 +11,8 @@ const { getPostCollection, getSpecificPost, getUserPostCollection } = require('.
 router.get('/api/post/collection/all', getPostCollection);
 router.get('/api/post/collection/:id', getUserPostCollection);
 router.get('/api/post/:id', getSpecificPost);
-router.post('/api/post/new', createNewPost);
 router.post('/api/post/:id', editSpecificPost);
+router.post('/api/post/new', createNewPost);
 router.post('/api/post/delete/:id', deleteSpecificPost)
 
 /////////////////////////////////////////
