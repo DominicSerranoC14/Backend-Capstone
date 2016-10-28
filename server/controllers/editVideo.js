684b51adae05e4f6daa5698a6ba64a0e7a73838b
Dominic Serrano <dmsrojo@gmail.com>
2016-10-28 15:58:50 -0500

    Can now receive static video file from RPI and view video file in browser

--- server/controllers/editVideo.js
+++ server/controllers/editVideo.js
@@ -1,23 +1,31 @@
 'use strict';
 
-const { writeFileSync, createReadStream, createWriteStream } = require('fs');
+const { createWriteStream } = require('fs');
+const exec = require('child_process').exec;
+const spawn = require('child_process').spawn;
 /////////////////////////////////////////
+
+const encodeVideo = (req, res) => {
+  return new Promise((resolve, reject) => {
+    req.pipe(createWriteStream('./server/videoFiles/video.h264'));
+    resolve();
+  });
+}
 
 
 const receiveVideoStream = (req, res) => {
 
-  req.pipe(createWriteStream('./server/videoFiles/video.h264'));
-
-};
-
-
-const sendVideoStream = (req, res) => {
-
-  createReadStream('./server/videoFiles/video.h264')
-  .pipe(res);
+  encodeVideo(req, res)
+  .then(() => {
+    setTimeout(() => {
+      exec(`ffmpeg -r 30 -i server/videoFiles/video.h264 -vcodec copy server/videoFiles/video.mp4`);
+      console.log('File video.h264 endcoded into video.mp4');
+    }, 6000);
+  })
+  .catch((err) => console.log('Error: ', err));
 
 };
 
 
 /////////////////////////////////////////
-module.exports = { receiveVideoStream, sendVideoStream };
+module.exports = { receiveVideoStream };
