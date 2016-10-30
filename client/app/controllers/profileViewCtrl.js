03ba2d17b2f00e0b5de669128e7e43b303c355ca
Dominic Serrano <dmsrojo@gmail.com>
2016-10-29 21:07:06 -0500

    Image display bar can now scroll on hover

--- client/app/controllers/profileViewCtrl.js
+++ client/app/controllers/profileViewCtrl.js
@@ -2,6 +2,41 @@
 
 app.controller('profileViewCtrl', function ($scope, $http) {
 
+  const rightArrow = $('#right-arrow');
+  const leftArrow = $('#left-arrow');
+  const imageDisplay = $('#profile-image-display');
+  let scroll;
 
+  const scrollLeft = () => {
+    scroll = setInterval(() => {
+      imageDisplay.animate({
+        scrollLeft: `+=${15}px`
+      });
+    }, 400)
+  };
+
+  const scrollRight = () => {
+    scroll = setInterval(() => {
+      imageDisplay.animate({
+        scrollLeft: `-=${15}px`
+      });
+    }, 400)
+  };
+
+  const stopScroll = () => {
+    clearInterval(scroll);
+  };
+
+  rightArrow.hover(() => {
+    scrollLeft();
+  }, () => {
+    stopScroll();
+  });
+
+  leftArrow.hover(() => {
+    scrollRight();
+  }, () => {
+    stopScroll();
+  });
 
 });
